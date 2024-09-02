867fcf0e0c343c5ff0df713447656ddc3fe00ffb
Soumik Majumder <soumikcode02@gmail.com>
2024-09-02 18:56:23 +0530

    done

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import "dotenv/config"
+import axios from "axios"
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get('/api/check', (req, res) => {
+    res.status(201).json({success: true, message: "Hello World!"});
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server running on port: ${PORT}`);
+})
+
+
+setInterval(() => {
+    axios.get(process.env.URL);
+}, 600000)
